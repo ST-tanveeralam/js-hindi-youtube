c6364f4654321162207ec698de1fcbe62ec5aee6
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-02 18:36:24 +0000

    conversion of string

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+//"33" =>33
+//"33abc" =>NaN
+//true =>1; fals =>0
+
+let isLogged = true
+
+let booleanIsLoggedIn = Boolean(isLogged)
+console.log(booleanIsLoggedIn)
+
+
+//1 => true ; 0 => false
+//""=> false
+//"tanu" => true
+
+let numberAge =25
+
+let valueInString = String(numberAge)
+console.log(valueInString)
+console.log( typeof valueInString)
