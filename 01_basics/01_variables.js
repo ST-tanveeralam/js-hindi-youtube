995d721690b048d52e1b1f7dfa5c7a8137f53525
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-02 17:10:25 +0000

    change some thing

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1;
+let accountEmail =" tanu@gmail.com";
+var accountCity ="Delhi";
+//newDelhi
+
+//accountId = 2;
+console.log(accountId);
+
+accountEmail =" tannu@gmail.com"
+accountCity="Bangaluru"
+//Jaynagar;
+console.table([accountId,accountEmail,accountCity])
