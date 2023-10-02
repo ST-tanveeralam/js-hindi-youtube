c74893aa3f1b57bfba58a4057603455ce6e767e8
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-02 17:19:21 +0000

    change the values

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,17 @@
 const accountId = 1;
 let accountEmail =" tanu@gmail.com";
-var accountCity ="Delhi";
-//newDelhi
+var accountPassword =12345;
+accountCity="newDelhi"
+let accountState
 
+/*
+prefer not use var because of issue in block scope and functional scope
+
+*/
 //accountId = 2;
 console.log(accountId);
 
 accountEmail =" tannu@gmail.com"
-accountCity="Bangaluru"
-//Jaynagar;
-console.table([accountId,accountEmail,accountCity])
+accountPassword=1212121
+accountCity="Jaynagar";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
