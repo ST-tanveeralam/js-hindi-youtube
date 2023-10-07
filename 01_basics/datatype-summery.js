e89713e82f725fcd5f9f0cd1ccffbc1273545bcc
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-07 10:13:45 +0000

    datatype summery

---
+++ 01_basics/datatype-summery.js
@@ -0,0 +1,32 @@
+//primitive 
+
+//7 types: string , number, boolean, null, undefined, symbole, BigInt
+const score =100;
+const scoreValue =100.6
+let city= null
+
+console.log(typeof city);
+
+
+
+
+//refrence (non primitive)
+// array, object,function
+
+const hero =["shakti,an","naagaraj", "dogo"];
+
+console.log(typeof hero)
+
+let myObj ={
+    name:"tanu",
+    age: 26,
+    city: "delhi"
+
+}
+console.log(typeof myObj);
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof myFunction)
