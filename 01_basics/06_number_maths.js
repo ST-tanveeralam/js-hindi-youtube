b4be874f8ddfbf8f937219a680468a966262134c
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-08 10:13:37 +0000

    num and maths

---
+++ 01_basics/06_number_maths.js
@@ -0,0 +1,17 @@
+const score =100;
+console.log(score)
+
+const balance = new Number(200) 
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const otherNuber= 123.8982
+console.log(otherNuber.toPrecision(4));
+
+const hundreds =10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
