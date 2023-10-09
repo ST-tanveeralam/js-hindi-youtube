87a1e96a790490ff28a34c237931ddb0990ae3b0
Tanveer Alam <115448939+ST-tanveeralam@users.noreply.github.com>
2023-10-09 11:54:42 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+//array
+const arr=[1,2,3,4,5,6]
+const myarr=["hero" ,"movies","cast"]
+
+//array methods
+console.log("A" ,arr)
+const updateArry=arr.slice(1,3);
+console.log(updateArry)
+
+console.log("B", arr)
+const updateArry1= arr.splice(1,3);
+console.log(updateArry1);
+
+console.log(arr); // main arry me change aaya hai "splice method ke wejah  se "
